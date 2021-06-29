2ff3325325d73a38dc8a331bac5e8ff784160fa9
Tuxette Chix <tuxette@chix.nathalievilla.org>
2021-06-29 11:43:38 +0100

    2nd commit

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const _ = require('lodash')
-const items = [1,[2,[3,[4,]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+const http = require('http');
+
+const server=http.createServer((req,res)=>{
+if(req.url === '/'){
+    for(let i =0; i<1000;i++)
+    {
+        for(let j=0 ; j<1000;j++){
+            console.log(`${i} ${j}`)
+        }
+    }
+    res.end('home page')
+}
+if(req.url ==='/about'){
+    res.end('about page')
+}
+
+})
+
+server.listen(5000,()=>{
+    console.log('server is listening on port : 5000')
+})
